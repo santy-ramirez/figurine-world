a483e54fa57edb10d1ecfe72ed2fa5a5eae0ee6a
santiago <santysjr956@gmail.com>
2022-10-28 20:33:28 -0300

    C009 fix update

--- src/components/Croper.jsx
+++ src/components/Croper.jsx
@@ -1,79 +1,77 @@
-import { useEffect, useState } from "react";
-import Cropper from "react-cropper";
-import "cropperjs/dist/cropper.css";
-import axios from "axios";
-import image from "../assets/MESSI.png";
+import { useEffect, useState } from 'react'
+import Cropper from 'react-cropper'
+import 'cropperjs/dist/cropper.css'
+import axios from 'axios'
+import image from '../assets/MESSI.png'
 
-const YOUR_API_KEY = "cg7Wpi47wPgwdaoevqcanJ21";
-const defaultSrc = image;
-
-
+const YOUR_API_KEY = 'cg7Wpi47wPgwdaoevqcanJ21'
+const defaultSrc = image
 
 function Coper(props) {
-  const [image2, setImage] = useState(defaultSrc);
-  const [cropper, setCropper] = useState(null);
-  if (!image2) {
-    return <h1>londing..</h1>;
-  }
-
-  const onChange = (e) => {
-    e.preventDefault();
-    let files;
-    if (e.dataTransfer) {
-      files = e.dataTransfer.files;
-    } else if (e.target) {
-      files = e.target.files;
+    const [image2, setImage] = useState(defaultSrc)
+    const [cropper, setCropper] = useState(null)
+    if (!image2) {
+        return <h1>londing..</h1>
     }
-    const reader = new FileReader();
-    reader.onload = () => {
-      setImage(reader.result);
-    };
-    reader.readAsDataURL(files[0]);
-  };
 
-  const removeBg = () => {
-    const formData = new FormData();
-    formData.append("size", "auto");
-    formData.append("image_file", image2);
-    axios({
-      method: "post",
-      url: "https://api.remove.bg/v1.0/removebg",
-      data: formData,
+    const onChange = (e) => {
+        e.preventDefault()
+        let files
+        if (e.dataTransfer) {
+            files = e.dataTransfer.files
+        } else if (e.target) {
+            files = e.target.files
+        }
+        const reader = new FileReader()
+        reader.onload = () => {
+            setImage(reader.result)
+        }
+        reader.readAsDataURL(files[0])
+    }
 
-      // responseType: "arraybuffer",
-      headers: {
-        ...formData,
-        "X-Api-Key": YOUR_API_KEY,
-      },
-      encoding: null,
-    })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((e) => console.log(e));
-  };
+    const removeBg = () => {
+        const formData = new FormData()
+        formData.append('size', 'auto')
+        formData.append('image_file', image2)
+        axios({
+            method: 'post',
+            url: 'https://api.remove.bg/v1.0/removebg',
+            data: formData,
 
-  useEffect(() => {
-    removeBg();
-  }, []);
+            // responseType: "arraybuffer",
+            headers: {
+                ...formData,
+                'X-Api-Key': YOUR_API_KEY,
+            },
+            encoding: null,
+        })
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((e) => console.log(e))
+    }
 
-  return (
-    <div className="coper">
-      <input type="file" onChange={onChange} />
-      <Cropper
-        style={{ height: 300, width: "100%" }}
-        zoomTo={0.5}
-        src={image2}
-        preview=".image-in"
-        background={false}
-        responsive={true}
-        onInitialized={(instance) => {
-          setCropper(instance);
-        }}
-        guides={true}
-      />
-    </div>
-  );
+    useEffect(() => {
+        removeBg()
+    }, [])
+
+    return (
+        <div className="coper">
+            <input type="file" onChange={onChange} />
+            <Cropper
+                style={{ height: 300, width: '100%' }}
+                zoomTo={0.8}
+                src={image2}
+                preview=".image-in"
+                background={false}
+                responsive={true}
+                onInitialized={(instance) => {
+                    setCropper(instance)
+                }}
+                guides={true}
+            />
+        </div>
+    )
 }
 
-export default Coper;
+export default Coper
